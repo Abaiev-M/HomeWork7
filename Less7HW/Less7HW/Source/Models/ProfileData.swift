3d45284bb9f226ba8a287a0cc46fbfa1256d3d7c
Abaiev-M <abaievmisha@gmail.com>
2024-12-11 19:44:32 +0200

    Done

---
+++ Less7HW/Less7HW/Source/Models/ProfileData.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileData.swift
+//  Less7HW
+//
+//  Created by Misha Abaiev on 11.12.2024.
+//
+
+protocol ProfileData {
+    
+    var avatarName: String { get }
+    var firstName: String { get }
+    var lastName: String { get }
+    var email: String { get }
+    var adderss: String { get }
+}
--- Less7HW/Less7HW/Source/Screens/ProfileViewController.swift
+++ Less7HW/Less7HW/Source/Screens/ProfileViewController.swift
@@ -23,21 +23,43 @@ class ProfileViewController: UIViewController {
     }
     
     @IBAction func loadUserDataButtonAction(_ sender: UIButton) {
-        
-        dataProvider.loadUserData()
+        dataProvider.loadUserData {[weak self] profileData in
+            if let profileVeiviewController = self {
+                profileVeiviewController.updateUI(data: profileData)
+            }
+        }
     }
     
     @IBAction func loadAdminDataButtonAction(_ sender: UIButton) {
-        
-        dataProvider.loadAdminData()
+        dataProvider.loadAdminData {[weak self] profileData in
+            if let profileVeiviewController = self {
+                profileVeiviewController.updateUI(data: profileData)
+            }
+        }
     }
     
-    func updateUI() {
-        
-//        avaImageView.image = UIImage(named: "")
-//        firstNameLabel.text =
-//        lastNameLabel.text =
-//        eMailLabel.text =
-//        addressLabel.text =
+    func updateUI(data: ProfileData) {
+        avaImageView.image = UIImage(named: "\(data.avatarName)")
+        firstNameLabel.text = data.firstName
+        lastNameLabel.text = data.lastName
+        eMailLabel.text = data.email
+        addressLabel.text = data.adderss
     }
+}
+
+
+extension UserModel: ProfileData {
+    var avatarName: String { return self.userAvatarName }
+    var firstName: String { return self.userFirstName }
+    var lastName: String { return self.userLastName }
+    var email: String { return self.userEmail }
+    var adderss: String { return self.userEmail }
+}
+
+extension AdminModel: ProfileData {
+    var avatarName: String { return self.adminAvatarName }
+    var firstName: String { return self.adminFirstName }
+    var lastName: String { return self.adminLastName }
+    var email: String { return self.adminEmail }
+    var adderss: String { return self.adminEmail }
 }
